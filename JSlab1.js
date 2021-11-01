05288479a5435c65824dda804769175371476343
Nick <nhamer233@gmail.com>
2021-10-31 21:44:43 -0400

    Changed while loop to while(true)

--- JSlab1.js
+++ JSlab1.js
@@ -24,18 +24,18 @@ for (let i = 0; i < lifeEvents.length; i++) {
 }
 
 let counter = 0;
-let randomNumber = 0;
 
-while (randomNumber !== 5) {
-  randomNumber = Math.floor(Math.random() * 10 + 1);
+while (true) {
+  let randomNumber = Math.floor(Math.random() * 10 + 1);
 
   if (randomNumber !== 5) {
     counter++;
-    console.log(`randomNumber !== 5`);
+    console.log(`${randomNumber} !== 5`);
   } else {
     counter++;
     console.log(
-      `5 === 5. it took ${counter} iterations to randomly generate the number 5`
+      `${randomNumber} === 5. it took ${counter} iterations to randomly generate the number 5`
     );
+    break;
   }
 }
